f98c1400684372766ff3b1c6f94a660b75ce9904
Philippe Négrel-Jerzy <philippe.negrel-jerzy@kanop.io>
2022-06-20 12:51:56 +0000

    style: update default text in template

--- src/DefaultPage.tsx
+++ src/DefaultPage.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import app from 'assets/app.svg';
 import { SiGithubsponsors } from 'react-icons/si';
 
-export default function Default() {
+export default function DefaultPage() {
   return (
     <div className="default-app">
       <img src={app} className="profile-pic" alt="app icon" />
       <p>
-        <h1>Woops</h1>
+        <h1>Hello there</h1>
         {' '}
         <h3 className="text-secondary">
-          This app is under construction
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </h3>
       </p>
       <a
