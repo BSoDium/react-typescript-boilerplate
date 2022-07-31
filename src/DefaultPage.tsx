e2ffcb1aec3c02316516b22b9a8a30b3ed3b9a46
Philippe Négrel-Jerzy <philippe.negrel-jerzy@kanop.io>
2022-07-31 21:50:44 +0000

    feat: add layout stylesheets

--- src/DefaultPage.tsx
+++ src/DefaultPage.tsx
@@ -5,10 +5,10 @@ import { SiGithubsponsors } from 'react-icons/si';
 
 export default function DefaultPage() {
   return (
-    <div className="default-app">
+    <div className="default-app container flex-col spacing-md">
       <img src={app} className="profile-pic" alt="app icon" />
-      <p>
-        <h1>Hello there</h1>
+      <section className="container flex-col spacing-sm">
+        <h1><b>Hello there</b></h1>
         {' '}
         <h3 className="text-secondary">
           Edit
@@ -17,9 +17,9 @@ export default function DefaultPage() {
           {' '}
           and save to reload.
         </h3>
-      </p>
+      </section>
       <a
-        className="default-app-link standard-btn href soft"
+        className="default-app-link button standard href soft"
         href="https://github.com/sponsors/BSoDium"
         target="_blank"
         rel="noopener noreferrer"
